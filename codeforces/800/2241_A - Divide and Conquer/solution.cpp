9de50c129997be32da646c93cd69f4bbf35d022f
tareq-az <taraqaziz508@gmail.com>
2026-08-20 08:00:35 +0600

    Add Codeforces 2241A - Divide and Conquer

---
+++ codeforces/800/2241_A - Divide and Conquer/solution.cpp
@@ -0,0 +1,15 @@
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+ 
+        if(x%y==0){
+            cout<<"YES"<<endl;
+        }else cout<<"NO"<<endl;
+    }
+}
