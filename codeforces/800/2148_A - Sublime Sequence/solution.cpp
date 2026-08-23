d04f4bb27fd5c89a32db854e42feafa1d6cee0cd
tareq-az <taraqaziz508@gmail.com>
2026-08-23 23:22:25 +0600

    Add Codeforces 2148A - Sublime Sequence

---
+++ codeforces/800/2148_A - Sublime Sequence/solution.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        int x;
+        cin>>x>>n;
+        if(n%2==0){
+            cout<< 0 <<endl;
+            
+        }
+        else cout<<x<<endl;
+    }
+}
