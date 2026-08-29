4a99fd1cb38b3ae336d6852d079fe12878fbb7de
tareq-az <taraqaziz508@gmail.com>
2026-08-29 10:15:02 +0600

    Add Codeforces 2139A - Maple and Multiplication

---
+++ codeforces/800/2139_A - Maple and Multiplication/solution.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b;
+        cin>>a>>b;
+ 
+        if(a==b){
+            cout<<0<<endl;
+            continue;
+        }
+        else if(a>b){
+            if(a%b==0){
+             cout<<1<<endl;
+            }
+            else cout<<2<<endl;
+        }
+        else if(b>a){
+            if(b%a==0){
+                cout<<1<<endl;
+            }else cout<<2<<endl;
+        }
+    }
+}
